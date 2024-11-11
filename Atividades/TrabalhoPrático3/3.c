93cb581f946704b86d55da8109c9bad16529cefb
Gustavo Maia <gustavommilitaoo@gmail.com>
2024-11-11 18:50:42 +0000

    Questao3

--- Atividades/TrabalhoPrático3/3.c
+++ Atividades/TrabalhoPrático3/3.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
 
-int main()
-{
-    int vezesJogadas;
-    int resultados[7] = {0, 0, 0, 0, 0, 0, 0};
-    int resultados2[7] = {0, 0, 0, 0, 0, 0, 0};
-    int i;
+int main(){
+    int vezesJogadas; // Quantas vezes vou testar
+    int resultados[100]; // Registrando quantos resultados eu quiser até 100
+    int i; // Para o laço
+    int contador[6] = {0, 0, 0, 0, 0, 0}; // Para adicionar quantas vezes, nas 6 possibilidades, se repetiram
 
-    printf("Digite quantas vezes você vai jogar: ");
+
+    // ENTRADA
+    printf("Digite quantas vezes você irá jogar: ");
     scanf("%d", &vezesJogadas);
 
-    for (i = 1; i <= vezesJogadas; i++)
+    for (i = 0; i < vezesJogadas; i++)
     {
-        printf("Na jogada %d, o resultado foi: ", i);
+        printf("Na jogada %d o resultado foi: ", i+1);
         scanf("%d", &resultados[i]);
     }
 
-    for (i = 1; i <= vezesJogadas; i++)
+
+    // CONTANDO QUAIS SE REPETIRAM
+    for (i = 0; i <= vezesJogadas; i++)
     {
-        resultados2[i] == resultados[i];
+        if (resultados[i] >= 1 && resultados[i] <= 6)
+        {
+            contador[resultados[i]]++;
+        }
+        
     }
-
-    // for (i = 1; i <= vezesJogadas; i++)
-    // {
-
-    // }
+    
+    // SAÍDA 
+    for (i = 1; i <=6; i++)
+    {
+        printf("A face %d repetiu %dx\n", i, contador[i]);
+    }
+    
+    return 0;
 }
