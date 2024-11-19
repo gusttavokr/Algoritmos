4cdf6efe8909ffe47d3b9a78dd75b382889c3b47
Gustavo Maia Militão <20241014040006@ifrn.local>
2024-11-19 17:07:09 -0300

    Tp2-q1.c

--- Atividades/TrabalhoPrático2/1.c
+++ Atividades/TrabalhoPrático2/1.c
@@ -1,9 +1,60 @@
 #include <stdio.h>
 
-int main(){
-    int operador;
+int main()
+{
+    int operador = 0;
+    double saldo = 0;
+    double deposito;
+    double sacar;
 
     printf("Seja bem-vindo(a) ao seu caixa eletronico\nDigite o digito da operacao:\n");
-    printf("1- Depositar, 2- Sacar, 3- Ver saldo\n");
-    scanf("%d ", &operador);
+    printf("1- Depositar, 2- Sacar, 3- Ver saldo, 4- Sair\n");
+
+    while (1)
+    {
+        scanf("%d", &operador);
+
+        if (operador == 1)
+        {
+            printf("Quanto voce deseja depositar?\n");
+            scanf("%lf", &deposito);
+            saldo = saldo + deposito;
+            printf("Voce depositou: R$%.2f\n", deposito);
+        }
+        else if (operador == 2)
+        {
+            if (saldo <= 0)
+            {
+                printf("Saldo insuficiente\n");
+            }
+            else
+            {
+                printf("Quanto voce deseja sacar?\n");
+                scanf("%lf", &sacar);
+                if (sacar > saldo)
+                {
+                    printf("Saldo insuficiente\n");
+                }
+                else
+                {
+                    saldo = saldo - sacar;
+                    printf("Voce sacou: R$%.2f\n", sacar);
+                }
+            }
+        }
+        else if (operador == 3)
+        {
+            printf("Seu saldo atual: R$%.2f\n", saldo);
+        }
+        else if (operador == 4)
+        {
+            printf("Agradecemos por usar o nosso banco!");
+            break;
+        }
+        if (operador != 4)
+        {
+            printf("1- Depositar, 2- Sacar, 3- Ver saldo, 4- Sair\n");
+        }
+    }
+    return 0;
 }
