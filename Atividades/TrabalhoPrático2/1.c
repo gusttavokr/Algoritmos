fd2705e09d85c94c9b352259f4cdeeb525653101
Gustavo Maia Militão <20241014040006@ifrn.local>
2024-11-19 14:54:29 -0300

    Tp2

--- Atividades/TrabalhoPrático2/1.c
+++ Atividades/TrabalhoPrático2/1.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    
+    int operador;
+
+    printf("Seja bem-vindo(a) ao seu caixa eletronico\nDigite o digito da operacao:\n");
+    printf("1- Depositar, 2- Sacar, 3- Ver saldo\n");
+    scanf("%d ", &operador);
 }
