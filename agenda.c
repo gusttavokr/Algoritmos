2003c7badc36b589d6753f8bcb25d4ad976f48d3
Gustavo Maia <gustavommilitaoo@gmail.com>
2024-12-07 20:07:31 +0000

    inserir_listar

--- agenda.c
+++ agenda.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct Contato
 {
@@ -7,43 +8,63 @@ struct Contato
     char numero[15];
 };
 
-struct Contato* inserir_contato(char nome[], char numero[])
-{
-    struct Contato* contato = (struct Contato*) malloc(sizeof(struct Contato));
-    
+void inserir_contato(struct Contato* contato, char nome[], char numero[])
+{    
     strcpy(contato->nome, nome);
     strcpy(contato->numero, numero);
 
-    return contato;
-};
-
-
+}
 
 int main(){
     int numCont;
     int op = 0;
     int opEntrada;
+    int i= 0;
 
-    struct Contato contatos[100];
+    struct Contato agenda[100];
+    int tamanho = sizeof(agenda);
 
     printf("Digite quantos contatos sua agenda tera: ");
     scanf("%d", &numCont);
 
     printf("Bem-vindo a Agenda de Contato\n");
-    while (op != 5)
-    {
+    while (op != 5){
         printf("Digite a operacao que deseja fazer:\n\n");
         printf("1- Inserir contato, 2 - Listar contatos, 3- Remover contato, 4- Atualizar contato, 5- Sair\n");
         scanf("%d", &opEntrada);
         op += opEntrada;
 
         if (op==1){
-            for (i = 0; i < sizeof(); i++)
-            {
-                /* code */
+            if (i==numCont){
+                printf("Agenda cheia\n");
             }
-            
+            if (i < numCont){
+        
+                char nome[100];
+                char numero[15];
+
+                printf("Digite o nome: ");
+                scanf("%s", nome);
+                printf("Digite o número: ");
+                scanf("%s", numero);
+
+                inserir_contato(&agenda[i], nome, numero);
+                i++;
+
+                printf("Contato: %s\n", nome);
+                printf("Numero: %s\n", numero);
+            }
+            op = 0;
         }
+
+        if (op == 2){
+            for (int j = 0; j < numCont; j++){
+                printf("\nContato: %s\n", agenda[j].nome);
+                printf("Numero: %s\n", agenda[j].numero);
+            }
+            op = 0;
+        }
+    }
         // if (op == 1)
         // {
         //     char nome[100];
@@ -72,7 +93,6 @@ int main(){
         // }
         
         
-    }
-    
     return 0;
 }
+    
